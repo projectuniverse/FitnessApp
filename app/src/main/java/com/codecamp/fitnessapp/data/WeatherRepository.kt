ca45baa6da7eb245356c17470ac19e18f2a8bff2
Julian Krüger <116820352+projectuniverse@users.noreply.github.com>
2023-02-20 11:44:04 +0100

    Added Weather Repo

---
+++ app/src/main/java/com/codecamp/fitnessapp/data/WeatherRepository.kt
@@ -0,0 +1,53 @@
+package com.codecamp.fitnessapp.data
+
+import com.codecamp.fitnessapp.model.Weather
+import com.codecamp.fitnessapp.network.WeatherApiService
+import kotlinx.coroutines.flow.first
+
+/*
+ * Acts as a single source of truth. Returns online and offline weather.
+ */
+interface WeatherRepository {
+    suspend fun getOnlineWeather(
+        latitude: Double,
+        longitude: Double
+    ): Weather
+    suspend fun getOfflineWeather(): Weather?
+}
+
+@kotlinx.serialization.ExperimentalSerializationApi
+class DefaultWeatherRepository(
+    private val weatherApiService: WeatherApiService,
+    private val weatherDao: WeatherDao
+) : WeatherRepository {
+
+    override suspend fun getOnlineWeather(
+        latitude: Double,
+        longitude: Double
+    ): Weather {
+        val deserializedWeather = weatherApiService.getWeather(latitude, longitude)
+        val weather = Weather(
+            weatherType = deserializedWeather.weather[0].main,
+            temperature = deserializedWeather.main.temp,
+            windSpeed = deserializedWeather.wind.speed,
+            time = deserializedWeather.dt,
+            sunrise = deserializedWeather.sys.sunrise,
+            sunset = deserializedWeather.sys.sunset,
+            cityName = deserializedWeather.name
+        )
+        if (!weatherDao.weatherExists().first()) {
+            weatherDao.insert(weather)
+        }
+        else {
+            weatherDao.update(weather)
+        }
+        return weather
+    }
+
+    override suspend fun getOfflineWeather(): Weather? {
+        if (weatherDao.weatherExists().first()) {
+            return weatherDao.getWeather(0).first()
+        }
+        return null
+    }
+}
---
+++ app/src/main/java/com/codecamp/fitnessapp/network/WeatherApiService.kt
@@ -0,0 +1,17 @@
+package com.codecamp.fitnessapp.network
+
+import com.codecamp.fitnessapp.model.DeserializedWeather
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/*
+ * Makes the request to the weather server
+ */
+interface WeatherApiService {
+    @GET("data/2.5/weather")
+    suspend fun getWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double,
+        @Query("appid") appid: String = "087957b907143749bd6d8c356211f383"
+    ): DeserializedWeather
+}
