77b0ea70db0c50ee39bb934170f6a48d31c84489
Julian Krüger <116820352+projectuniverse@users.noreply.github.com>
2023-02-22 15:39:25 +0100

    added weatherviewmodel

---
+++ app/src/main/java/com/codecamp/fitnessapp/ui/screens/dashboard/WeatherViewModel.kt
@@ -0,0 +1,21 @@
+package com.codecamp.fitnessapp.ui.screens.dashboard
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.codecamp.fitnessapp.data.weather.DefaultWeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel
+@Inject constructor(
+    private val weatherRepository: DefaultWeatherRepository,
+) : ViewModel() {
+    val weather = weatherRepository.weather
+    init {
+        viewModelScope.launch {
+            weatherRepository.fetchWeather(51.31,9.48)
+        }
+    }
+}
