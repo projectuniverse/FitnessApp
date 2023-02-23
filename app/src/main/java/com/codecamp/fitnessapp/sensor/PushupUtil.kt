599052806b994be155ca7a462b4a787950eff54f
Niklas Koch <nk1000@gmx.de>
2023-02-23 14:50:52 +0100

    start: pushup util

--- app/src/main/java/com/codecamp/fitnessapp/sensor/PushupUtil.kt
+++ app/src/main/java/com/codecamp/fitnessapp/sensor/PushupUtil.kt
@@ -1,9 +1,36 @@
 package com.codecamp.fitnessapp.sensor
 
-class PushupUtil {
+import androidx.compose.runtime.mutableStateOf
+import kotlinx.coroutines.flow.MutableStateFlow
 
-    /*** Wenn Proximity:
-     *      -einheit in cm oder bool?
-     *
-    */
+
+enum class PushupState {
+    START,
+    DOWN
+}
+class PushupUtil {
+    var state = mutableStateOf(PushupState.START)
+    val repetitions = MutableStateFlow(0)
+    private val threshold = 1.0f
+    val x = 0
+    fun name() {
+        when(state.value) {
+            PushupState.START -> {
+                //check if sensor data falls under threshold
+                // yes -> change state to DOWN
+                if (x < threshold) {
+                    state.value = PushupState.DOWN
+                }
+            }
+            PushupState.DOWN -> {
+                //check if sensor data falls above threshold
+                // yes -> change state to START
+                //        increase repetitions
+                if (x >= threshold) {
+                    state.value = PushupState.START
+                    repetitions.value++
+                }
+            }
+        }
+    }
 }
