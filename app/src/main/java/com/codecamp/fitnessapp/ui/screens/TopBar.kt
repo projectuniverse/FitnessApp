99b79bfceaf8331bbdddf1aa2c4b28387fb243ce
Julian Krüger <116820352+projectuniverse@users.noreply.github.com>
2023-02-27 20:10:30 +0100

    Fixed error where top bar overlayed content

--- app/src/main/java/com/codecamp/fitnessapp/ui/screens/TopBar.kt
+++ app/src/main/java/com/codecamp/fitnessapp/ui/screens/TopBar.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
-import com.codecamp.fitnessapp.R
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
