f178f9a8fba08cdb4f002302a7a1cf345d73a800
Julian Krüger <116820352+projectuniverse@users.noreply.github.com>
2023-03-04 17:56:25 +0100

    Updated Altitude Graph Colors

--- app/src/main/java/com/codecamp/fitnessapp/ui/screens/result/AltitudeResult.kt
+++ app/src/main/java/com/codecamp/fitnessapp/ui/screens/result/AltitudeResult.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -16,11 +17,13 @@ import com.patrykandpatrick.vico.compose.chart.line.lineChart
 import com.patrykandpatrick.vico.compose.component.textComponent
 import com.patrykandpatrick.vico.compose.style.ChartStyle
 import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
+import com.patrykandpatrick.vico.compose.style.currentChartStyle
 import com.patrykandpatrick.vico.core.axis.AxisPosition
 import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
 import com.patrykandpatrick.vico.core.axis.horizontal.HorizontalAxis
-import com.patrykandpatrick.vico.core.chart.line.LineChart
-import com.patrykandpatrick.vico.core.entry.*
+import com.patrykandpatrick.vico.core.chart.copy
+import com.patrykandpatrick.vico.core.entry.ChartEntry
+import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import java.math.RoundingMode
 import kotlin.math.ceil
 
@@ -42,35 +45,26 @@ fun AltitudeResult(
             contentColor = MaterialTheme.colorScheme.onSurface,
         )
     ) {
-        ProvideChartStyle(
-            chartStyle = ChartStyle.fromColors(
-                axisGuidelineColor = MaterialTheme.colorScheme.onSurface,
-                axisLabelColor = MaterialTheme.colorScheme.onSurface,
-                axisLineColor = MaterialTheme.colorScheme.onSurface,
-                entityColors = listOf(MaterialTheme.colorScheme.onSurfaceVariant),
-                elevationOverlayColor = MaterialTheme.colorScheme.errorContainer
-            )
-        ) {
-            class Entry(
-                val time: Int,
-                override val x: Float,
-                override val y: Float,
-            ) : ChartEntry {
-                override fun withY(y: Float) = Entry(time, x, y)
-            }
+        /*
+         * TODO: Sample data set. Replace with the data set given in constructor.
+         */
+        val values: MutableList<Pair<Int, Float>> = mutableListOf()
+        val start = 1677633370
+        for (i in 0 until 720) {
+            values.add(start + i*10 to (0..20).random().toFloat())
+        }
 
-            /*
-             * Sample data set. Replace with the data set given in constructor.
-             */
-            var values: MutableList<Pair<Int, Float>> = mutableListOf()
-            var start = 1677633370
-            for (i in 0 until 720) {
-                values.add(start + i*10 to (0..20).random().toFloat())
-            }
+        class Entry(
+            val time: Int,
+            override val x: Float,
+            override val y: Float,
+        ) : ChartEntry {
+            override fun withY(y: Float) = Entry(time, x, y)
+        }
 
-            var startTime = 0
-            val chartEntryModel =
-                values
+        var startTime = 0
+        val chartEntryModel =
+            values
                 .mapIndexed { index, (time, height) ->
                     if (index == 0) {
                         startTime = time
@@ -82,42 +76,47 @@ fun AltitudeResult(
                 }
                 .let { ChartEntryModelProducer(it) }
 
-            val axisValueFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
-                (chartValues.chartEntryModel.entries.first().getOrNull(value.toInt()) as? Entry)
-                    ?.run {
-                        if (time == 0) {
-                            "0"
-                        } else {
-                            "${(time / 60f).toBigDecimal().setScale(2, RoundingMode.UP).toDouble()}"
-                        }
+        val axisValueFormatter = AxisValueFormatter<AxisPosition.Horizontal.Bottom> { value, chartValues ->
+            (chartValues.chartEntryModel.entries.first().getOrNull(value.toInt()) as? Entry)
+                ?.run {
+                    if (time == 0) {
+                        "0"
+                    } else {
+                        "${(time / 60f).toBigDecimal().setScale(2, RoundingMode.UP).toDouble()}"
                     }
-                    .orEmpty()
+                }
+                .orEmpty()
+        }
+
+        /*
+         * Denotes at what interval x-axis values should be displayed.
+         * E.g., chartSpacing = 18 means every third minute will be displayed
+         * on the x-axis (so 0, 3, 6, 9, ...) - because Track objects are created
+         * every 10 sec so there are 18 track Objects created in 3 minutes.
+         * Although only every third minute is displayed on the x-axis,
+         * ALL height-values are in the graph (not just every 18th).
+         */
+        val chartSpacing =
+            if (values.size < 7) {
+                1
+            }
+            else {
+                6 * ceil((values.size / 6) / 10f).toInt()
             }
 
-            /*
-             * Denotes at what interval x-axis values should be displayed.
-             * E.g., chartSpacing = 18 means every third minute will be displayed
-             * on the x-axis (so 0, 3, 6, 9, ...) - because Track objects are created
-             * every 10 sec so there are 18 track Objects created in 3 minutes.
-             * Although only every third minute is displayed on the x-axis,
-             * ALL height-values are in the graph (not just every 18th).
-             */
-            val chartSpacing =
-                if (values.size < 7) {
-                    1
-                }
-                else {
-                    6 * ceil((values.size / 6) / 10f).toInt()
-                }
-
+        ProvideChartStyle(
+            chartStyle = ChartStyle.fromColors(
+                axisGuidelineColor = MaterialTheme.colorScheme.onSurface,
+                axisLabelColor = MaterialTheme.colorScheme.onSurface,
+                axisLineColor = MaterialTheme.colorScheme.onSurface,
+                entityColors = listOf(MaterialTheme.colorScheme.onSurfaceVariant),
+                elevationOverlayColor = MaterialTheme.colorScheme.errorContainer
+            )
+        ) {
+            val defaultLines = currentChartStyle.lineChart.lines
             Chart(
                 chart = lineChart(
-                    lines = listOf(
-                        LineChart.LineSpec(
-                            pointSizeDp = 1f,
-                            //lineColor = 4284612846
-                        )
-                    ),
+                    lines = remember(defaultLines) { listOf(defaultLines.first().copy(pointSizeDp = 1f)) },
                     spacing = 3.dp
                 ),
                 model = chartEntryModel.getModel(),
