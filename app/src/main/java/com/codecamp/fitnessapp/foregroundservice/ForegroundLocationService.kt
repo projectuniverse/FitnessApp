0b8ccd2627027bb8e92c54bc8b81a6835b9b5c3a
Niklas Koch <nk1000@gmx.de>
2023-03-13 09:40:28 +0100

    add to locationtracker, start on foregroundservice

---
+++ app/src/main/java/com/codecamp/fitnessapp/foregroundservice/ForegroundLocationService.kt
@@ -0,0 +1,49 @@
+package com.codecamp.fitnessapp.foregroundservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.codecamp.fitnessapp.location.LocationTrackerInterface
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
+import javax.inject.Inject
+
+class ForegroundLocationService @Inject constructor(
+    private val locationTracker: LocationTrackerInterface
+): Service() {
+
+    private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action) {
+            ACTION_START -> start()
+            ACTION_STOP -> stop()
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start() {
+
+    }
+
+    private fun stop() {
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    companion object {
+        const val ACTION_START = "ACTION_START"
+        const val ACTION_STOP = "ACTION_STOP"
+    }
+}
--- app/src/main/java/com/codecamp/fitnessapp/location/LocationTrackerImplementation.kt
+++ app/src/main/java/com/codecamp/fitnessapp/location/LocationTrackerImplementation.kt
@@ -1,13 +1,23 @@
 package com.codecamp.fitnessapp.location
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.os.Looper
 import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.coroutines.resume
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -16,10 +26,28 @@ class LocationTrackerImplementation @Inject constructor(
     private var fusedLocationClient: FusedLocationProviderClient,
     private val app: Application
 ): LocationTrackerInterface {
+
+    private fun hasPermissions(): Boolean {
+        val locationManager = app.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val gpsEnabled = locationManager.isProviderEnabled(Context.LOCATION_SERVICE) or
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val fineLocationPermission = ContextCompat.
+        checkSelfPermission(app, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        val coarseLocationPermission = ContextCompat.
+        checkSelfPermission(app, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        if (!gpsEnabled || !fineLocationPermission || !coarseLocationPermission) {
+            return false
+        }
+
+        return true
+    }
+
+    @SuppressLint("MissingPermission")
     override suspend fun getLocation(): Location? {
 
         // check for necessary permissions
-        val locationManager = app.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        /*val locationManager = app.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val gpsEnabled = locationManager.isProviderEnabled(Context.LOCATION_SERVICE) or
                             locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
         val fineLocationPermission = ContextCompat.
@@ -28,6 +56,10 @@ class LocationTrackerImplementation @Inject constructor(
             checkSelfPermission(app, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
         if (!gpsEnabled || !fineLocationPermission || !coarseLocationPermission) {
+            return null
+        }*/
+
+        if (!hasPermissions()) {
             return null
         }
 
@@ -47,6 +79,41 @@ class LocationTrackerImplementation @Inject constructor(
                 addOnCanceledListener {
                     continuation.cancel()
                 }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun getLocationFlow(rate: Long): Flow<Location> {
+        return callbackFlow {
+            if (!hasPermissions()) {
+                throw Exception("Missing location permissions")
+            }
+
+            val locationManager = app.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            val isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+            if (!isGpsEnabled && !isNetworkEnabled) {
+                throw Exception("Gps and network disabled")
+            }
+
+            //val request = LocationRequest.create().setInterval(rate).setFastestInterval(rate)
+            val request = LocationRequest.Builder(rate).build()
+
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(p0: LocationResult) {
+                    super.onLocationResult(p0)
+                    p0.locations.lastOrNull()?.let { location ->
+                        launch { send(location) }
+                    }
+                }
+            }
+
+            fusedLocationClient.requestLocationUpdates(request, locationCallback, Looper.getMainLooper())
+
+            awaitClose {
+                fusedLocationClient.removeLocationUpdates(locationCallback)
             }
         }
     }
