5cadabd65ee3203ec2ca99b87429d5e2dea7ddf0
Niklas Koch <nk1000@gmx.de>
2023-03-13 09:40:28 +0100

    add to locationtracker, start on foregroundservice

--- app/src/main/java/com/codecamp/fitnessapp/location/LocationTrackerInterface.kt
+++ app/src/main/java/com/codecamp/fitnessapp/location/LocationTrackerInterface.kt
@@ -1,7 +1,10 @@
 package com.codecamp.fitnessapp.location
 
 import android.location.Location
+import kotlinx.coroutines.flow.Flow
 
 interface LocationTrackerInterface {
     suspend fun getLocation(): Location?
+
+    fun getLocationFlow(rate: Long): Flow<Location>
 }
