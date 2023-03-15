13d2a526412715c307cfa8f65298ef662cf2cf09
Niklas Koch <nk1000@gmx.de>
2023-03-15 16:37:40 +0100

    changes to health connect

--- app/src/main/java/com/codecamp/fitnessapp/healthconnect/HealthConnect.kt
+++ app/src/main/java/com/codecamp/fitnessapp/healthconnect/HealthConnect.kt
@@ -15,6 +15,10 @@ class HealthConnect @Inject constructor(
     var healthConnectClient: HealthConnectClient? = null
 
     fun getHealthConnectClient() {
+        if (healthConnectClient != null) {
+            return
+        }
+
         if(!HealthConnectClient.isApiSupported()) {
             return
         }
@@ -24,7 +28,8 @@ class HealthConnect @Inject constructor(
         }
     }
 
-    suspend fun getStepsRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<StepsRecord>? {
+    /*suspend fun getStepsRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<StepsRecord>? {
+        getHealthConnectClient()
         if (healthConnectClient == null) {
             return null
         }
@@ -35,9 +40,9 @@ class HealthConnect @Inject constructor(
                 timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
             )
         )
-    }
+    }*/
 
-    suspend fun getActiveCaloriesBurnedRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<ActiveCaloriesBurnedRecord>? {
+    /*suspend fun getActiveCaloriesBurnedRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<ActiveCaloriesBurnedRecord>? {
         if (healthConnectClient == null) {
             return null
         }
@@ -48,9 +53,9 @@ class HealthConnect @Inject constructor(
                 timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
             )
         )
-    }
+    }*/
 
-    suspend fun getExerciseSessionRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<ExerciseSessionRecord>? {
+    suspend fun getExerciseSessionRecords(): ReadRecordsResponse<ExerciseSessionRecord>? {
         /***
          *  useful constants:
          *      EXERCISE_TYPE_BIKING = 8
@@ -60,7 +65,7 @@ class HealthConnect @Inject constructor(
          *
          *      EXERCISE_TYPE_OTHER_WORKOUT = 0
          */
-
+        getHealthConnectClient()
 
         if (healthConnectClient == null) {
             return null
@@ -69,12 +74,12 @@ class HealthConnect @Inject constructor(
         return healthConnectClient!!.readRecords(
             ReadRecordsRequest(
                     ExerciseSessionRecord::class,
-                    timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
+                    timeRangeFilter = TimeRangeFilter.before(Instant.now())
             )
         )
     }
 
-    suspend fun getHeightRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<HeightRecord>? {
+    /*suspend fun getHeightRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<HeightRecord>? {
         if (healthConnectClient == null) {
             return null
         }
@@ -85,9 +90,9 @@ class HealthConnect @Inject constructor(
                 timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
             )
         )
-    }
+    }*/
 
-    suspend fun getWeightRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<WeightRecord>? {
+    /*suspend fun getWeightRecords(startTime: Instant, endTime: Instant): ReadRecordsResponse<WeightRecord>? {
         if (healthConnectClient == null) {
             return null
         }
@@ -98,5 +103,5 @@ class HealthConnect @Inject constructor(
                 timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
             )
         )
-    }
+    }*/
 }
