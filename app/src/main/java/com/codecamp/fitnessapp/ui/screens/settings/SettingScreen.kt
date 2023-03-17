12662b61b2b1e1a3647289f995c5f4b9cb363f51
Niklas Koch <nk1000@gmx.de>
2023-03-17 12:49:00 +0100

    health connect fixes

--- app/src/main/java/com/codecamp/fitnessapp/ui/screens/settings/SettingScreen.kt
+++ app/src/main/java/com/codecamp/fitnessapp/ui/screens/settings/SettingScreen.kt
@@ -1,6 +1,5 @@
 package com.codecamp.fitnessapp.ui.screens.settings
 
-import android.content.Intent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -8,24 +7,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ActivityCompat
-import androidx.health.connect.client.permission.HealthPermission
-import androidx.health.connect.client.records.ExerciseSessionRecord
-import androidx.health.connect.client.records.HeightRecord
-import androidx.health.connect.client.records.WeightRecord
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.codecamp.fitnessapp.MainActivity
 import com.codecamp.fitnessapp.R
-import com.codecamp.fitnessapp.healthconnect.PermissionHealthConnectActivity
-import com.codecamp.fitnessapp.ui.screens.result.AltitudeResult
+import com.codecamp.fitnessapp.model.healthconnect.HealthConnectUsage
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberMultiplePermissionsState
-import com.google.accompanist.permissions.rememberPermissionState
 import kotlinx.coroutines.flow.MutableStateFlow
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -34,7 +23,8 @@ fun SettingScreen(
     settingViewModel: SettingsViewModel = hiltViewModel()
 ) {
     val user = settingViewModel.user.collectAsState(initial = null)
-    val hcPermission = settingViewModel.hcPermission.collectAsState(initial = null)
+    val hcUsageState = settingViewModel.hcPermission.collectAsState(initial = HealthConnectUsage(0, false, false))
+
     val stringSettings = stringArrayResource(R.array.settings)
     // If value is null, null.toString() returns "null" (not valid value)
     var age = MutableStateFlow(if (user.value?.age.toString() == "null") "" else user.value?.age.toString())
@@ -81,9 +71,11 @@ fun SettingScreen(
                 /*TODO Google Health Connect*/
                 //TODO: HC Permission
                 settingViewModel.updateHealthConnectUsage()
-                if (hcPermission.value?.useHealthConnect == true && hcPermission.value?.hasPermission == true) {
-                    isValidValues.value = settingViewModel.checkForHealthConnectData(age.value, height.value, weight.value)
-                }
+                isValidValues.value = settingViewModel.checkForHealthConnectData(
+                    age.value,
+                    height.value,
+                    weight.value
+                )
             }
         ) {
             Text(
