30afb15418a9a0b52fa06eb185a382b4b712ebc0
Niklas Koch <nk1000@gmx.de>
2023-03-26 12:46:39 +0200

    code cleanup

--- app/src/main/java/com/codecamp/fitnessapp/location/LocationTrackerImplementation.kt
+++ app/src/main/java/com/codecamp/fitnessapp/location/LocationTrackerImplementation.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.os.Looper
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.getSystemService
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -45,19 +46,6 @@ class LocationTrackerImplementation @Inject constructor(
 
     @SuppressLint("MissingPermission")
     override suspend fun getLocation(): Location? {
-
-        // check for necessary permissions
-        /*val locationManager = app.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val gpsEnabled = locationManager.isProviderEnabled(Context.LOCATION_SERVICE) or
-                            locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        val fineLocationPermission = ContextCompat.
-            checkSelfPermission(app, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-        val coarseLocationPermission = ContextCompat.
-            checkSelfPermission(app, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-
-        if (!gpsEnabled || !fineLocationPermission || !coarseLocationPermission) {
-            return null
-        }*/
 
         if (!hasPermissions()) {
             return null
