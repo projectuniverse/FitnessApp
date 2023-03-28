54d74d9f7a31a20522a58e5e2b48e2e57f91ca3a
finnmerkes <70281329+finnmerkes@users.noreply.github.com>
2023-03-28 02:23:32 +0200

    Outside Done
    +Starting Location a current location

--- app/src/main/java/com/codecamp/fitnessapp/ui/screens/outside/Map.kt
+++ app/src/main/java/com/codecamp/fitnessapp/ui/screens/outside/Map.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
@@ -11,26 +12,20 @@ import com.google.maps.android.compose.*
 
 @Composable
 fun Map (
-    latlngList: List<LatLng>
+    latlngList: List<LatLng>,
+    start: LatLng
 ) {
     val zoom = 17f
-    val start = LatLng(51.3204621,9.4886897)
-
-//    val trackList = viewModel.trackList.collectAsState(initial = null).value
 
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(start, zoom)
     }
 
-
-    // val locationSource: LocationSource =
-
     GoogleMap(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 5.dp),
-        cameraPositionState = cameraPositionState,
-//        locationSource = locationSource
+        cameraPositionState = cameraPositionState
     ) {
         if (latlngList != null && latlngList.isNotEmpty()) {
             Polyline(points = latlngList)
